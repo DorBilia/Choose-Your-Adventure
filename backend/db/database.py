0347768f9b269b6d30fa03a31920d23e2ae5ed07
dorbi <dor.bilia2@gmila.com>
2026-04-15 21:19:59 +0300

    fixed import bug

--- backend/db/database.py
+++ backend/db/database.py
@@ -1,11 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
-from backend.core.config import settings
+from core.config import settings
 
 engine = create_engine(settings.DATABASE_URL)
 
-SessionLocal = sessionmaker(autocommits=False, autoflush=False, bind=engine)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
 
